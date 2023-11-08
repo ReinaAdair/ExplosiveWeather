93ab8c910c5e45f40b857014243adefe69d35c5a
Reina Adair <vincintgordon@gmail.com>
2023-11-07 20:34:22 -0500

    later bruh

--- APICaller.py
+++ APICaller.py
@@ -1,27 +1,56 @@
 import json
 import requests
-import PySimpleGUI as sg
+#import PySimpleGUI as sg
 
-location = "indianapolis"
-apikey = "9xi9ZXm6xNUKDVQDS6DQKsiwtuJMJb61"
-units= "imperial"
-calltype = "realtime"
+def Prebuilt_API_Call():
+    location = "indianapolis"
+    apikey = "9xi9ZXm6xNUKDVQDS6DQKsiwtuJMJb61"
+    units = "imperial"
+    calltype = "realtime"
 
-#URL used to call the API
-url = f'https://api.tomorrow.io/v4/weather/{calltype}?location={location}&units={units}&apikey={apikey}'
+    #URL used for calling the API
+    url = f'https://api.tomorrow.io/v4/weather/{calltype}?location={location}&units={units}&apikey={apikey}'
 
-#Headers used for calling the API
-headers = {"accept": "application/json"}
+    #Headers used for calling the API
+    headers = {"accept": "application/json"}
 
-#Variable for holding the response recieved and the actual request to the API
-response = requests.get(url, headers=headers)
+    #Assigining API response to variable
+    response = requests.get(url, headers=headers)
 
-r = json.loads(response.text)
+    r = json.loads(response.text)
 
-for val in r["data"]["values"]:
-    print(val, ": ", r["data"]["values"][val])
+    try:
+        for val in r["data"]["values"]:
+            print(val, ": ", r["data"]["values"][val])
+    except:
+        print("An error has occured parsing the JSON")
+        print(r)
+        return "error"
 
+    for val in r["data"]["values"]:
+        if val == "humidity":
+            humidity = r["data"]["values"][val]
+        elif val == "precipitationProbability":
+            precipitationProbability = r["data"]["values"][val]
+        elif val == "pressureSurfaceLevel":
+            pressureSurfaceLevel = r["data"]["values"][val]
+        elif val == "temperature":
+            temperature = r["data"]["values"][val]
+        elif val == "temperatureApparent":
+            temperatureApparent = r["data"]["values"][val]
+        elif val == "weatherCode":
+            weatherCode = r["data"]["values"][val]
+        elif val == "windSpeed":
+            windSpeed = r["data"]["values"][val]
 
+    return humidity, precipitationProbability, pressureSurfaceLevel, temperature, temperatureApparent, weatherCode, windSpeed
+
+def API_Call(location, units, calltype):
+    pass
+
+Prebuilt_API_Call()
+
+"""Add a try catch for the API call in case of exceeding call limit"""
 
 """
 values returned in json format from the API
@@ -34,21 +63,21 @@ data:{
         cloudCover:
         dewPoint:
         freezingRainIntensity:
-        humidity:
-        precipitationProbability:
-        pressureSurfaceLevl:
+        humidity: ###
+        precipitationProbability: ###
+        pressureSurfaceLevl: ###
         rainIntensity:
         sleetIntensity:
         snowIntensity:
-        temperature:
-        temeratureApparent:
+        temperature: ###
+        temeratureApparent: ###
         uvHealthConcern:
         uvIndex:
         visibility:
         weatherCode:
         windDirection:
         windGust:
-        windSpeed:
+        windSpeed: ###
     }
     location:{
         lat:
